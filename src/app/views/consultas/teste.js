9f4a5e312b4fdd438d7a45129a6cc4384dd1b371
LeandroSannt <lsn_slim@yahoo.com.br>
2021-01-17 18:28:56 -0300

    Deixando os valores recebidos dinamicos

---
+++ src/app/views/consultas/teste.js
@@ -0,0 +1,13 @@
+function mult(value){
+    var x , y
+    x = (40/100) * value
+    y = (60/100) * value
+       
+    document.querySelector("#n1").value = x
+    document.querySelector("#n2").value = y
+
+}
+
+
+
+
