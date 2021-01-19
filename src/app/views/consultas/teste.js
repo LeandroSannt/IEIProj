7bc1104a3c1b675173b21dc64a1cd63a5d9a6018
LeandroSannt <lsn_slim@yahoo.com.br>
2021-01-19 20:12:28 -0300

    Tela de financeiro

--- src/app/views/consultas/teste.js
+++ src/app/views/consultas/teste.js
@@ -1,10 +1,9 @@
 function mult(value){
-    var x , y
-    x = (40/100) * value
-    y = (60/100) * value
+   const instituto = (40/100) * value
+   const profissional = (60/100) * value
        
-    document.querySelector("#n1").value = x
-    document.querySelector("#n2").value = y
+    document.querySelector("#n1").value = profissional
+    document.querySelector("#n2").value = instituto
 
 }
 
