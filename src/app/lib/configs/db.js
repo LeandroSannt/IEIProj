e5129ea69761d9452a5d2d0e1b49bcd2a78fb437
LeandroSannt <lsn_slim@yahoo.com.br>
2021-01-26 12:58:02 -0300

    alterando rota do edit

--- src/app/lib/configs/db.js
+++ src/app/lib/configs/db.js
@@ -2,7 +2,7 @@ const {Pool} = require ("pg")
 
 module.exports = new Pool({
     user:"postgres",
-    password:"3047",
+    password:"postgres",
     host:"localhost",
     port:5432,
     database:"IEIProj"
